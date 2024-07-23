ffcbaa80bc1a541a630563441c7b7fb2878a6a75
Pratoosh-18 <pratoosh10garg@gmail.com>
2024-07-23 21:50:22 +0530

    Updated carousel

--- src/components/Home/Carousel.jsx
+++ src/components/Home/Carousel.jsx
@@ -1,29 +1,39 @@
-import React from "react";
+import React from 'react';
+import { Carousel } from 'react-responsive-carousel';
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
-import { Carousel } from "react-responsive-carousel";
-import "./SchoolCarousel.css";
 
 const SchoolCarousel = () => {
   return (
-    <Carousel
-      showArrows={true}
-      autoPlay={true}
-      infiniteLoop={true}
-      showThumbs={false}
-      showStatus={false}
-      interval={3000}
-      className="carousel"
-    >
-      <div>
-        <img src="https://via.placeholder.com/800x400" alt="Event 1" />
-      </div>
-      <div>
-        <img src="https://via.placeholder.com/800x400" alt="Event 2" />
-      </div>
-      <div>
-        <img src="https://via.placeholder.com/800x400" alt="Event 3" />
-      </div>
-    </Carousel>
+    <div className="relative">
+      <Carousel
+        showArrows={true}
+        autoPlay={true}
+        infiniteLoop={true}
+        showThumbs={false}
+        showStatus={false}
+        interval={3000}
+        className="carousel"
+      >
+        <div>
+          <img src="https://via.placeholder.com/800x400" alt="Event 1" />
+          <div className="text-sm md:text-base w-full absolute bottom-4 left-1/2 transform -translate-x-1/2 px-4 py-2 rounded translate-y-[-10px]">
+            Annual Sports Day - Celebrating Excellence in Sports
+          </div>
+        </div>
+        <div>
+          <img src="https://via.placeholder.com/800x400" alt="Event 2" />
+          <div className="text-sm md:text-base w-full absolute bottom-4 left-1/2 transform -translate-x-1/2 px-4 py-2 rounded translate-y-[-10px]">
+            Science Exhibition - Showcasing Student Innovations
+          </div>
+        </div>
+        <div>
+          <img src="https://via.placeholder.com/800x400" alt="Event 3" />
+          <div className="text-sm md:text-base w-full absolute bottom-4 left-1/2 transform -translate-x-1/2 px-4 py-2 rounded translate-y-[-10px]">
+            Cultural Fest - Embracing Diversity and Creativity
+          </div>
+        </div>
+      </Carousel>
+    </div>
   );
 };
 
